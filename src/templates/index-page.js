420211dd79cc8251b1bb441efc0231563c20c8e6
czhk555 <116976032+czhk555@users.noreply.github.com>
2025-01-02 01:00:37 +0800

    Update index-page.js

--- src/templates/index-page.js
+++ src/templates/index-page.js
@@ -1,103 +1,51 @@
-import React, { Suspense } from 'react'
-// import PropTypes from "prop-types";
-import { graphql } from "gatsby";
+import React from "react"
+import { graphql } from "gatsby"
 import Layout from "../components/layout"
 import Seo from "../components/seo"
-import { GatsbyImage, getImage } from "gatsby-plugin-image"
-import { useLocation } from '@reach/router'
-
-// eslint-disable-next-line
-const PostCard = React.lazy(() => import('../components/postCard'))
+import PostCard from "../components/postCard"
 
 const IndexPage = ({ data }) => {
-  const location = useLocation()
-  const canonical = `${data.site.siteMetadata.siteUrl}${location.pathname}`
-  const siteTitle = data.site.siteMetadata.title
-  const social = data.site.siteMetadata.social
   const posts = data.allMarkdownRemark.edges
   let postCounter = 0
-  const image = getImage(data.markdownRemark.frontmatter.thumbnail)
 
   return (
-    <Layout title={siteTitle} social={social}>
-      <Seo
-        keywords={[`Gatsby Theme`, `Free Gatsby Template`, `Clay Gatsby Theme`]}
-        title={data.markdownRemark.frontmatter.title}
-        description={data.markdownRemark.frontmatter.description || ''}
-        image={data.markdownRemark.frontmatter.thumbnail.childImageSharp.gatsbyImageData.images.fallback.src}
-        pathname={location.pathname}
-        canonical={canonical}
-        schemaMarkup={{
-          '@context': 'https://schema.org',
-          '@type': 'WebPage',
-          name: data.markdownRemark.frontmatter.title,
-          description: data.markdownRemark.frontmatter.description
-        }}
-      />
-      <GatsbyImage
-        image={image}
-        alt={data.markdownRemark.frontmatter.title}
-        loading="lazy"
-      />
+    <Layout>
+      <Seo title="Home" />
       <div className="post-feed">
-        <Suspense fallback={<div>Loading...</div>}>
-          {posts.map(({ node }) => {
-            postCounter++
-            return (
-              <PostCard
-                key={node.fields.slug}
-                count={postCounter}
-                node={node}
-                postClass={`post`}
-              />
-            )
-          })}
-        </Suspense>
+        {posts.map(({ node }) => {
+          postCounter++
+          return (
+            <PostCard
+              key={node.fields.slug}
+              count={postCounter}
+              node={node}
+              postClass={`post`}
+            />
+          )
+        })}
       </div>
     </Layout>
   )
 }
+
 export default IndexPage
-export const IndexPageQuery = graphql`
-  query IndexPage {
-    site {
-      siteMetadata {
-        title
-        social {
-          twitter
-          facebook
-        }    
-      }
-    }
-    markdownRemark(frontmatter: {templateKey: {eq: "index-page"}}) {
-      frontmatter {
-        title
-        description
-        thumbnail {
-          childImageSharp {
-            gatsbyImageData(
-              width: 1360
-              formats: [AUTO, WEBP, AVIF]
-              placeholder: BLURRED
-              layout: CONSTRAINED
-            )
-          }
-        }
-      }
-    }
+
+export const pageQuery = graphql`
+  query {
     allMarkdownRemark(
-      filter: {frontmatter: {pagetype: {eq: "main"}}}
-      limit: 30
-      sort: {frontmatter: {number: ASC}}
+      filter: { frontmatter: { templateKey: { eq: "work-sub-page" } } }
+      sort: { frontmatter: { date: DESC } }
     ) {
       edges {
         node {
+          excerpt
           fields {
             slug
           }
           frontmatter {
-            date(formatString: "MMMM DD,YYYY")
             title
+            templateKey
+            date(formatString: "MMMM DD, YYYY")
             description
             thumbnail {
               childImageSharp {
@@ -114,4 +62,4 @@ export const IndexPageQuery = graphql`
       }
     }
   }
-`;
+`
