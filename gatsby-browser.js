8962d9f9ac7671c74e061a61d7f8087e820fc4c8
czhk555 <116976032+czhk555@users.noreply.github.com>
2025-01-03 01:20:43 +0800

    3.3.7

--- gatsby-browser.js
+++ gatsby-browser.js
@@ -4,3 +4,8 @@ import "typeface-merriweather"
 import "typeface-nunito"
 import "typeface-alegreya"
 
+// Prismjs theme
+import "prismjs/themes/prism.css"
+import "prismjs/plugins/line-numbers/prism-line-numbers.css"
+import "prismjs/plugins/command-line/prism-command-line.css"
+
