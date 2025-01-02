2c24abba4bfbed971c49175e1420829e4c63dac0
czhk555 <116976032+czhk555@users.noreply.github.com>
2025-01-03 02:51:10 +0800

    3.3.9

--- src/templates/blog-post.js
+++ src/templates/blog-post.js
@@ -1,77 +1,48 @@
 import React from "react"
-import { graphql } from "gatsby"
-import { GatsbyImage, getImage } from "gatsby-plugin-image";
-import LeftIcon from '../img/left-icon.svg'
-import RightIcon from '../img/right-icon.svg'
+import { Link, graphql } from "gatsby"
 import Layout from "../components/layout"
 import SEO from "../components/seo"
 
-const BlogPostTemplate = (props) => {
- 
-  const { pageContext } = props
-  const nextSlug = pageContext.next ? pageContext.next.fields.slug.split('/').slice(2, -1).join('/') === '' ? '/' :
-    `/${pageContext.next.fields.slug.split('/').slice(2, -1).join('/')}` : '/'
-  const previousSlug = pageContext.previous ? pageContext.previous.fields.slug.split('/').slice(2, -1).join('/') === '' ? '/' :
-    `/${pageContext.previous.fields.slug.split('/').slice(2, -1).join('/')}` : "/"
-  const nextLinkStatus = pageContext.next ? pageContext.next.frontmatter.templateKey === 'blog-post' ? true : false : false
-  const previousLinkStatus = pageContext.previous ? pageContext.previous.frontmatter.templateKey === 'blog-post' ? true : false : false
+const BlogPostTemplate = ({ data, pageContext, location }) => {
+  const post = data.markdownRemark
+  const { previous, next } = pageContext
 
-  const post = props.data.markdownRemark
-  const siteTitle = props.data.site.siteMetadata.title
-  const social = props.data.site.siteMetadata.social
   return (
-    <Layout location={props.location} title={siteTitle} social={social}>
+    <Layout>
       <SEO
         title={post.frontmatter.title}
         description={post.frontmatter.description || post.excerpt}
-        image={post.frontmatter.thumbnail.childImageSharp.gatsbyImageData.images.fallback.src}
-
       />
-      <article
-        className={`post-content ${post.frontmatter.thumbnail || `no-image`}`}
-      >
-        <header className="post-content-header">
-          <h1 className="post-content-title">{post.frontmatter.title}</h1>
+      <article>
+        <header>
+          <h1>{post.frontmatter.title}</h1>
+          <p>{post.frontmatter.date}</p>
         </header>
-        {post.frontmatter.description && (
-          <p className="post-content-excerpt">{post.frontmatter.description}</p>
-        )}
-        {post.frontmatter.thumbnail && (
-          <div className="post-content-image">
-            <GatsbyImage
-              // image={post.frontmatter.thumbnail.childImageSharp.gatsbyImageData}
-              // className="kg-image"
-              image={getImage(post.frontmatter.thumbnail)}
-              className="kg-image"
-              alt={post.frontmatter.title} />
-          </div>
-        )}
-        <div
-          className="post-content-body"
-          dangerouslySetInnerHTML={{ __html: post.html }}
-        />
-        <div className="post-link">
-          <div>
-          <a style={{ display: nextLinkStatus ? "flex" : 'none', alignItems: "center", color: "#131313", fontSize: "2rem" }} href={nextSlug} >
-              <img src={LeftIcon} alt='' width={30} height={30} />
-              <span>{pageContext.next ? pageContext.next.frontmatter.title : ""}
-              </span>
-            </a>
-
-          </div>
-          <div>
-          <a style={{ display: previousLinkStatus ? "flex" : 'none', alignItems: "center", color: "#131313", fontSize: "2rem" }} href={previousSlug}>
-              <span>{pageContext.previous ? pageContext.previous.frontmatter.title : ""}
-              </span>
-              <img src={RightIcon} alt='' width={30} height={30} />
-            </a>
-
-          </div>
-        </div>
+        <div dangerouslySetInnerHTML={{ __html: post.html }} />
+        <hr />
+        <footer>
+          <nav>
+            <ul>
+              <li>
+                {previous && (
+                  <Link to={previous.fields.slug} rel="prev">
+                    ← {previous.frontmatter.title}
+                  </Link>
+                )}
+              </li>
+              <li>
+                {next && (
+                  <Link to={next.fields.slug} rel="next">
+                    {next.frontmatter.title} →
+                  </Link>
+                )}
+              </li>
+            </ul>
+          </nav>
+        </footer>
       </article>
     </Layout>
-  );
-
+  )
 }
 
 export default BlogPostTemplate
