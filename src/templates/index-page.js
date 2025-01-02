f21f8138c61bc3c4d99eb6027f00e55299d13c6d
czhk555 <116976032+czhk555@users.noreply.github.com>
2025-01-03 03:30:36 +0800

    3.4.3

--- src/templates/index-page.js
+++ src/templates/index-page.js
@@ -1,40 +1,30 @@
-import React, { Suspense, lazy } from "react";
-import { graphql } from "gatsby";
+import React from "react";
+import { Link, graphql } from "gatsby";
 import Layout from "../components/layout";
-import Seo from "../components/seo";
+import SEO from "../components/seo";
+import PostCard from "../components/postCard";
 
-// 使用 React.lazy 来懒加载 PostCard 组件
-const PostCard = lazy(() => import("../components/postCard"));
-
-const IndexPage = ({ data }) => {
+const IndexPage = ({ data, location }) => {
   const siteTitle = data.site.siteMetadata.title;
   const social = data.site.siteMetadata.social;
   const posts = data.allMarkdownRemark.edges;
   let postCounter = 0;
 
   return (
-    <Layout title={siteTitle} social={social}>
-      <Seo
-        keywords={["Gatsby Theme", "Free Gatsby Template", "Clay Gatsby Theme"]}
-        title={data.markdownRemark.frontmatter.title}
-        description={data.markdownRemark.frontmatter.description || ""}
-        image={data.markdownRemark.frontmatter.thumbnail.childImageSharp.fluid.src}
-      />
+    <Layout location={location} title={siteTitle} social={social}>
+      <SEO title="All posts" />
       <div className="post-feed">
-        {/* 使用 Suspense 组件包裹懒加载的 PostCard 组件 */}
-        <Suspense fallback={<div>Loading posts...</div>}>
-          {posts.map(({ node }) => {
-            postCounter++;
-            return (
-              <PostCard
-                key={node.fields.slug}
-                count={postCounter}
-                node={node}
-                postClass="post"
-              />
-            );
-          })}
-        </Suspense>
+        {posts.map(({ node }) => {
+          postCounter++;
+          return (
+            <PostCard
+              key={node.fields.slug}
+              count={postCounter}
+              node={node}
+              postClass="post"
+            />
+          );
+        })}
       </div>
     </Layout>
   );
