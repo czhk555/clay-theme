7d3f59f5f963848f285d916668083c23bb9fe346
czhk555 <116976032+czhk555@users.noreply.github.com>
2025-01-03 02:44:05 +0800

    3.3.8

--- src/pages/404.js
+++ src/pages/404.js
@@ -1,41 +1,20 @@
 import React from "react"
-import { graphql } from "gatsby"
+import { Link } from "gatsby"
 
 import Layout from "../components/layout"
 import SEO from "../components/seo"
 
-class NotFoundPage extends React.Component {
-  render() {
-    const { data } = this.props
-    const siteTitle = data.site.siteMetadata.title
-    const social = data.site.siteMetadata.social
-
-    return (
-      <Layout location={this.props.location} title={siteTitle} social={social}>
-        <SEO title="404: Not Found" />
-        <div className="good-bye-page">
-         <h1>Not Found</h1>
-         <p>You just hit a route that doesn&#39;t exist... 🪦</p>
-         <p><a href="/">Return Home</a>
-         </p>
-        </div>
-      </Layout>
-    )
-  }
+const NotFoundPage = () => {
+  return (
+    <Layout>
+      <SEO title="404: Not Found" />
+      <div style={{ textAlign: 'center', padding: '5rem 0' }}>
+        <h1>404: Not Found</h1>
+        <p>You just hit a route that doesn&#39;t exist... the sadness.</p>
+        <Link to="/">Go back to homepage</Link>
+      </div>
+    </Layout>
+  )
 }
 
 export default NotFoundPage
-
-export const pageQuery = graphql`
-  query {
-    site {
-      siteMetadata {
-        title
-        social{
-          twitter
-          facebook
-        }
-      }
-    }
-  }
-`
