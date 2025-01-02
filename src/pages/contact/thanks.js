304832398240ac4cdf25ba78f2f35f4aab3a625d
czhk555 <116976032+czhk555@users.noreply.github.com>
2025-01-03 03:01:13 +0800

    3.3.9

--- src/pages/contact/thanks.js
+++ src/pages/contact/thanks.js
@@ -1,46 +1,17 @@
-
 import React from "react"
-import { graphql } from "gatsby"
-
+import { Link } from "gatsby"
 import Layout from "../../components/layout"
 import SEO from "../../components/seo"
 
-const Thanks = (props) => {
-    const { data } = props
-    const siteTitle = data.site.siteMetadata.title
-    const social = data.site.siteMetadata.social
-
-    return (
-        <Layout location={props.location} title={siteTitle} social={social}>
-            <SEO title="Thank You | Form submitted successfully" />
-
-
-            <article className="post-content page-template no-image">
-              <div className="thank-you">
-                <div className="post-content-body">
-                    <h1>Thank You</h1>
-                    <p> Your form has been submitted Successfully</p>
-                    <p><a href="/">Return Home</a></p>
-                </div>
-              </div>
-            </article>
-        </Layout>
-    )
-
-}
+const Thanks = () => (
+  <Layout>
+    <SEO title="Thanks for your message" />
+    <div className="thanks-page">
+      <h1>Thank you!</h1>
+      <p>Your message has been sent successfully.</p>
+      <Link to="/">Return to homepage</Link>
+    </div>
+  </Layout>
+)
 
 export default Thanks
-
-export const pageQuery = graphql`
-  query {
-    site {
-      siteMetadata {
-        title
-        social{
-          twitter
-          facebook
-        }
-      }
-    }
-  }
-`
